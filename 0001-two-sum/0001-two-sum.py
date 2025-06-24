28e1b41432213bf8dbcfadbbcdf19a06c8bf3d18
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2025-06-24 11:25:43 +0200

    Time: 0 ms (100%), Space: 18.7 MB (62.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-    
+
         ref_dic = dict()
-        final_lst = list()
-
-        for i,num in enumerate(nums):
-            ref_dic[i] = num
-
-        for k,v in ref_dic.items():
-            residual = target - v
-            if residual in ref_dic.values():
-                compatible = [key for key,value in ref_dic.items() if value == residual and key != k]
-                if compatible:
-                    final_lst.append(k)
-                    final_lst.append(compatible[0])
-
-                    break
-    
+        for i, num in enumerate(nums):
+            residual = target - num
+            if residual in ref_dic:
+                final_lst = [ref_dic[residual], i]
+                break
+            ref_dic[num] = i
+                
         return final_lst
