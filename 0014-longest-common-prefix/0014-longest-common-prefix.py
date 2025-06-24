6beffaf2784c5309d6a6d7dc3636787bc13aacb6
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2025-06-24 16:35:14 +0200

    Time: 0 ms (100%), Space: 18.3 MB (8.19%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,13 +2,10 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
         ref_dic = dict()
-        min_len = min([len(word) for word in strs])
-        strs_adj = [x[:min_len] for x in strs]
 
-        for word in strs_adj:
-            print(word)
+        for word in strs:
             for i, char in enumerate(word):
-                if i not in ref_dic.keys():
+                if i not in ref_dic:
                     ref_dic[i] = [char]
                 else:
                     ref_dic[i].append(char)
@@ -16,11 +13,11 @@ class Solution:
         final = list()
 
         for v in ref_dic.values():
-            if len(set(v)) == 1:
+            if len(set(v)) == 1 and len(v) == len(strs):
                 final.append(set(v))
             else:
                 break
 
-        otp = ''.join([list(x)[0] for x in final])    
+        otp = ''.join([list(x)[0] for x in final]) 
     
         return otp
