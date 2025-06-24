00ba8d45ad6f61528e72dab34b63d5ffb4ef9fbd
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2025-06-24 15:03:33 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,37 +2,29 @@ class Solution:
     def romanToInt(self, s: str) -> int:
 
         mapping = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
 
         roman_number = s
         total = 0
-        _diff = 0
+        prev = 0
 
-        for i, char in enumerate(roman_number):
+        for char roman_number:
 
-            int_char = mapping[char]
-            total += int_char
-            total -= _diff
-            _diff = 0
+            current_value = mapping[char]
 
-            try:
+            if prev < current_value:
+                total += (current_value - prev * 2)
+            else:
+                total += current_value
 
-                if char == "C" and (roman_number[i+1] == "M" or roman_number[i+1] == "D"):
-                    _diff = 100*2
-                elif char == "X" and (roman_number[i+1] == "L" or roman_number[i+1] == "C"):
-                    _diff = 10*2
-                elif char == "I" and (roman_number[i+1] == "V" or roman_number[i+1] == "X"):
-                    _diff = 1*2
-
-            except IndexError:
-                break
+            prev = current_value
 
         return total
 
