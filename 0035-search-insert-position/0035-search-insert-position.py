73deeb678960352e174e794091a4c5854b529552
Massimiliano <144174500+MassiCanosi@users.noreply.github.com>
2025-06-26 19:40:57 +0200

    Time: 0 ms (100%), Space: 18.4 MB (43.28%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,21 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+
         _len = len(nums)
         pointer = 0
-        difference = 0
         otp = 0
 
         while pointer < _len:
-            residual = target - nums[pointer]
-            if residual == 0:
-                otp = pointer
+            if nums[pointer] >= target:
+                pointer
                 break
-            else:
-                prev_difference = residual
-                if prev_difference > difference:
-                    prev_difference = difference
-                    otp = pointer + 1
-
             pointer += 1
         
-        return otp
+        return pointer
